8bc12eac8ba79d51c5e140027c770a598bc99b46
Ben Rogers <brogers@echo28.com>
2017-01-06 11:41:27 -0800

    sessions

--- saber/config.js
+++ saber/config.js
@@ -1,16 +1,29 @@
 import rc from 'rc';
 
 let defaults = {
-    database: {
-        database: "sber",
-        username: "sber",
-        password: "sber",
-        options: {
-            host: "localhost",
-            port: "3306",
-            dialect: "mysql",
-        },
-    },
+	database: {
+		database: "sber",
+		username: "sber",
+		password: "sber",
+		options: {
+			host: "localhost",
+			port: "3306",
+			dialect: "mysql",
+		},
+	},
+	session: {
+		cookie: {
+			path:'/',
+			httpOnly: true,
+			secure: false,
+			maxAge: null,
+		},
+		secret: 'CHANGEME',
+		store: 'sequelize',
+		resave: false,
+		saveUninitialized: false,
+		rolling: true,
+	},
 };
 let config = rc('saber', defaults);
 export default config;
---
+++ saber/server.js
@@ -0,0 +1,30 @@
+'use strict';
+import express from 'express';
+import bodyParser from 'body-parser';
+import config from './config';
+import sequelize from './db';
+import session from 'express-session';
+import SessionSequelize from 'connect-session-sequelize';
+const SequelizeStore = SessionSequelize(session.Store);
+
+let expressObj = null;
+export function server() {
+	expressObj = express();
+	expressObj.use(bodyParser.json());
+	expressObj.use(bodyParser.urlencoded({extended: false}));
+	let sessionConfig = config.session;
+	if (sessionConfig.store == 'sequelize') {
+		sessionConfig.store = new SequelizeStore({db: sequelize});
+		sessionConfig.store.sync();
+	}
+	expressObj.use(session(sessionConfig));
+	return expressObj;
+}
+export function listen() {
+	expressObj.listen(3000, (err) => {
+		if (err) {
+			console.log(err);
+		}
+		console.info('==> Listening on port 3000.  Open up http://localhost:3000/ in your browser.');
+	});
+}
